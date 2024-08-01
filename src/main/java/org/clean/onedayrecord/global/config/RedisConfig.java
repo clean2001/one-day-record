e19c1c346664c1c72fde58df064dce0fb401b03e
Sejeong Kim <clearrworld@gmail.com>
2024-08-01 10:07:57 +0900

    chore: cache manager 설정

---
+++ src/main/java/org/clean/onedayrecord/global/config/RedisConfig.java
@@ -0,0 +1,63 @@
+package org.clean.onedayrecord.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+@EnableRedisRepositories
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final ObjectMapper objectMapper;
+    private final RedisProperties redisProperties;
+//    private final SentinelProperties redisSentinelProperties;
+
+    @Value("${spring.redis.host}") // org.springframework.beans.factory.annotation.Value;
+    public String host;
+
+    @Value("${spring.redis.port}")
+    public Integer port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port); // 이렇게되면 0번 DB로 들어감
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisStockTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // Object에는 주로 json이 들어옴
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // Json을 Serialize하기 위한 툴
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    //== 캐시 매니저 ==//
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+}
--- src/main/java/org/clean/onedayrecord/global/etc/InitialDataLoader.java
+++ src/main/java/org/clean/onedayrecord/global/etc/InitialDataLoader.java
@@ -7,6 +7,8 @@ import org.clean.onedayrecord.domain.member.repository.MemberRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Component
 public class InitialDataLoader implements CommandLineRunner {
@@ -14,6 +16,10 @@ public class InitialDataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("hello world!");
+
+        Optional<Member> adminOpt = memberRepository.findBySocialId("1");
+
+        if(adminOpt.isPresent()) return;
 
         Member admin = Member.builder()
                 .nickname("admin")
